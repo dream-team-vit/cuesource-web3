a93a96c95a6df8c4b41283de69d28482bb86c945
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-03-25 11:55:49 +0530

    added the basic crud for quest and bid with thirdweb

--- web3/contracts/Quest.sol
+++ web3/contracts/Quest.sol
@@ -4,11 +4,14 @@ pragma solidity ^0.8.9;
 //comment this if you don't want to use console.log
 // import "hardhat/console.sol";
 
-contract QuestCreator {
+import "./Ownable.sol";
+
+contract QuestCreator is Ownable {
 
 	struct Bid {
 		string iD;
 		string developerID;
+		address developerAddress;
 		string proposal;
 		uint16 bidAmmount;
 	}
@@ -24,13 +27,20 @@ contract QuestCreator {
 		uint16 deadlineDayCount;
 		uint16 issueId;
 		string selectedBid;
+		bool accepted;
+		uint selectedTime;
+		uint deadlineTime;
 		Bid[] bids;
 	}
-
 
 	mapping (uint => Quest) quests;
 
 	uint public numberOfQuests = 0;
+	uint private ammountInContract = 0;
+
+	function getTotalContractAmmount() public view onlyOwner() returns (uint) {
+		return ammountInContract;
+	}
 
 	function createQuest(string memory _description, uint _minPrize, uint _maxPrize, uint16 _deadlineDayCount, uint16 _issueId) public returns (uint) {
 		Quest storage quest = quests[numberOfQuests];
@@ -40,6 +50,7 @@ contract QuestCreator {
 		quest.minPrize = _minPrize;
 		quest.maxPrize = _maxPrize;
 		quest.description = _description;
+		quest.accepted = false;
 
 		numberOfQuests++;
 
@@ -81,11 +92,12 @@ contract QuestCreator {
 		return quest;
 	}
 
-	function createBid(uint _questId, string memory _newBidId, string memory _developerID, string memory _proposal, uint16 _bidAmmount) public returns  (uint) {
+	function createBid(uint _questId, string memory _newBidId, string memory _developerID, address _developerAddress, string memory _proposal, uint16 _bidAmmount) public returns  (uint) {
 		Bid memory bid;
 		
 		bid.iD = _newBidId;
 		bid.developerID = _developerID;
+		bid.developerAddress = _developerAddress;
 		bid.proposal = _proposal;
 		bid.bidAmmount = _bidAmmount;
 
@@ -124,12 +136,63 @@ contract QuestCreator {
 		return _bidId;
 	}
 
-	function selectBid(string memory _bidId, uint _questId) public returns (uint) {
+	function selectBid(string memory _bidId, uint _questId) payable public returns (uint) {
 		Quest storage quest = quests[_questId];
+		Bid memory bid;
 
+		for (uint i = 0; i < quest.bids.length; i++) {
+			if (keccak256(bytes(quest.bids[i].iD)) == keccak256(bytes(_bidId))) {
+				bid = quest.bids[i];
+				require(msg.value == bid.bidAmmount, "Please deposit the selected bid ammount");
+				ammountInContract += bid.bidAmmount;
+			}
+		}
+
+		// setting the selected bid to the bid selected by the org
 		quest.selectedBid = _bidId;
 
+		// setting the selectedTime and deadlineTime on quest
+		quest.selectedTime = block.timestamp;
+		quest.deadlineTime = _getDeadline(quest.selectedTime, quest.deadlineDayCount);
+
 		return _questId;
+	}
+
+	function _getDeadline(uint _selectedTime, uint16 _deadlineDayCount) private pure returns (uint) {
+		// Number of seconds in a day is 86400
+		uint deadlineDayCountSec = _deadlineDayCount * 86400;
+		uint deadlineDayTime = _selectedTime + deadlineDayCountSec;
+
+		return deadlineDayTime;
+	}
+
+	function getAccept(uint _questId) public view returns (bool) {
+		Quest memory quest = quests[_questId];
+
+		return quest.accepted;
+	}
+
+	function setAccept(bool newVal, uint _questId) public {
+		Quest storage quest = quests[_questId];
+		Bid memory selectedBid;
+
+		quest.accepted = newVal;
+		
+		for (uint i = 0; i < quest.bids.length; i++) {
+			if (keccak256(bytes(quest.bids[i].iD)) == keccak256(bytes(quest.selectedBid))) {
+				selectedBid = quest.bids[i];
+		}}
+
+		// if the work is not accepted the money is to be transferred back to the org
+		if (quest.accepted == false) {
+			(bool success, ) = payable(quest.owner).call{value: selectedBid.bidAmmount}("");
+
+			require(success, "The money should be transferred back to the organziation");
+		} else { // if the work is accepted the money is to be sent to the developer address
+			(bool success, ) = payable(selectedBid.developerAddress).call{value: selectedBid.bidAmmount}("");
+
+			require (success, "The money should be tranferred to the developer's address");
+		}
 	}
 
 	function viewBid(uint _questId, string memory _bidId) public view returns (Bid memory) {
